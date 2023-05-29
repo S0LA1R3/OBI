79ba7a3b9bffafdf8e4c29a6ef5aca6d16306fdc
Guilherme Braga <crazyboysfh3@gmail.com>
2023-05-29 18:22:37 -0300

    all right

--- cifra.py
+++ cifra.py
@@ -1,28 +1,30 @@
 alfa = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'z']
+consoante = []
+for x in alfa:
+    if x not in 'aeiou':
+        consoante += x
 a = alfa.index('a')
 e = alfa.index('e')
 i = alfa.index('i')
 o = alfa.index('o')
 u = alfa.index('u')
 palavra = input('Digite a palavra: ')
+c = 0
 for letra in palavra:
     letra2 = letra
-    if letra2 not in 'aeiou':
-        posicoes = sorted([a, e, i, o, u, alfa.index(letra2)])
-        if posicoes[posicoes.index(alfa.index(letra2))] - posicoes[posicoes.index(alfa.index(letra2))-1] < posicoes[posicoes.index(alfa.index(letra2))+1] - posicoes[posicoes.index(alfa.index(letra2))]:
-            letra2 += alfa[posicoes[posicoes.index(alfa.index(letra2))-1]]
-        elif posicoes[posicoes.index(alfa.index(letra2))] - posicoes[posicoes.index(alfa.index(letra2))-1] == posicoes[posicoes.index(alfa.index(letra2))+1] - posicoes[posicoes.index(alfa.index(letra2))]:
-            if alfa[posicoes[alfa.index(letra2)-1]] < alfa[posicoes[alfa.index(letra2)+1]]:
-                letra2 += alfa[posicoes[posicoes.index(alfa.index(letra2))-1]]
+    posicaoLetra = alfa.index(letra)
+    if letra not in 'aeiou':
+        posicoes = sorted([a, e, i, o, u, posicaoLetra])
+        if letra == 'z':
+            letra2 += alfa[posicoes[posicoes.index(posicaoLetra) - 1]] + 'z'
+        else:
+            if posicaoLetra - posicoes[posicoes.index(posicaoLetra) - 1] <= posicoes[
+                posicoes.index(posicaoLetra) + 1] - posicaoLetra:
+                letra2 += alfa[posicoes[posicoes.index(posicaoLetra) - 1]]
             else:
-                letra2 += alfa[posicoes[posicoes.index(alfa.index(letra2))+1]]
-        else:
-            letra2 += alfa[posicoes[posicoes.index(alfa.index(letra2))+1]]
-        if alfa[alfa.index(letra2[:len(letra2)-1])+2] not in 'aeiou':
-            letra2 += alfa[alfa.index(letra2[:len(letra2)-1])+1]
-        else:
-            letra2 += alfa[alfa.index(letra2[:len(letra2)-1])+2]
-    palavra = palavra.replace(letra, letra2)
-    print(palavra)
+                letra2 += alfa[posicoes[posicoes.index(posicaoLetra) + 1]]
+            letra2 += consoante[consoante.index(letra) + 1]
+        palavra = palavra.replace(palavra[c:], palavra[c:].replace(letra, letra2, 1))
+    c += 1
 print(palavra)
