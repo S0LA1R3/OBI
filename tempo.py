6ab4b0016b0226b679d3b49476b15a9d8d367db2
Guilherme Braga <crazyboysfh3@gmail.com>
2023-06-19 20:51:47 -0300

    update

--- tempo.py
+++ tempo.py
@@ -4,21 +4,28 @@ registros = []
 for c in range(quantidadeRegistros):
     registros.append(input())
 
-amigos = {}
-tempos = {}
+contagem = {}
 for x in registros:
-    if 'R' in x:
-        amigos[x.split()[1]] = False
-        tempos[x.split()[1]] = -2
-    elif 'E' in x:
-        amigos[x.split()[1]] = True
-    elif 'T' in x:
-        for y in amigos.keys():
-            if not amigos[y]:
-                tempos[y] += int(x.split()[1])
-    for y in amigos.keys():
-        if not amigos[y]:
-            tempos[y] += 1
+    tempo = 1
 
-for x in amigos.keys():
-    print(x, tempos[x])
+    if 'R' in x:
+        if x.split()[1] not in contagem.keys():
+            contagem[x.split()[1]] = 0
+        if contagem[x.split()[1]] >= 999999:
+            contagem[x.split()[1]] -= 999999
+
+    if 'T' in x:
+        tempo = int(x.split()[1])
+
+    for y in contagem.keys():
+        if not contagem[y] >= 999999 and 'R ' + y not in x:
+            contagem[y] += tempo
+
+    if 'E' in x:
+        contagem[x.split()[1]] += 999999
+
+for x in sorted(contagem.keys()):
+    if contagem[x] < 999999:
+        print(x, -1)
+    else:
+        print(x, contagem[x] - 999999)
