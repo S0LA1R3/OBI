89a2401719ca53632943904562cf182cf99d2d50
Guilherme Braga <crazyboysfh3@gmail.com>
2023-07-13 22:26:25 -0300

    update

---
+++ OBI/gravity&flip.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    int quantity;
+    int[quantity] numbers;
+
+    cin >> quantity;
+    cin
+    return 0
+}
